75346e4d1798fde4f8be5daf234c352dcc625330
anis kchaou <aniskchaou@gmail.com>
2020-07-11 02:04:19 +0200

    build

---
+++ build/precache-manifest.ae82426b0494309e64b1b5c0b11ed5eb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8776f05a2fd18f9865e915c1911887",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8874a2d9a420a0e4451e",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "08be41589b1a610b2540",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8874a2d9a420a0e4451e",
+    "url": "/static/js/2.9140856d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9140856d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08be41589b1a610b2540",
+    "url": "/static/js/main.1cb0dcbf.chunk.js"
+  },
+  {
+    "revision": "00542f638f49ee474eee",
+    "url": "/static/js/runtime-main.38b6b61a.js"
+  }
+]);
